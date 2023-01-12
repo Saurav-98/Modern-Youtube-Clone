2a947d5883b161daa975b1e4b99ba0cf23821909
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2023-01-12 17:54:08 +0530

    Feed Sidebar working

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import { Box, Stack, Typography } from '@mui/material';
+import SideBar from './SideBar';
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack
+      sx={{
+        flexDirection: { xs: 'column', md: 'row' },
+      }}
+    >
+      <Box
+        sx={{
+          // background: 'purple',
+          height: { xs: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { xs: 0, md: 2 },
+        }}
+      >
+        <SideBar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          Copyright 2023 Saurav Kumar Verma
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
